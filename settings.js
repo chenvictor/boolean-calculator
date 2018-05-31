4452c5a3021317d54fe705af4a16bdf166069242
Victor Chen <chenvictor1999@gmail.com>
2018-05-31 13:28:44 -0700

    Implemented Settings and Setting storage.

--- settings.js
+++ settings.js
@@ -6,4 +6,60 @@ const Settings = new function() {
   this.setForceParens = function(newBool) {
     forceParens = newBool;
   }
+
+  this.toggleCheckbox = function(checkbox) {
+    checkBoxDict[checkbox.id] = checkbox.checked;
+    if (checkbox.id == "selectAll") {
+      //set all except short
+      var short = checkBoxDict["short"];
+      for (var key in checkBoxDict) {
+        checkBoxDict[key] = checkbox.checked;
+      }
+      checkBoxDict["short"] = short;
+      sync();
+    }
+  }
+
+  this.getValue = function(key) {
+    return checkBoxDict[key];
+  }
+
+  var checkBoxDict = {
+    short: true,
+    com: true,
+    ass: true,
+    dist: false,
+    i: false,
+    neg: false,
+    dneg: false,
+    id: false,
+    ub: false,
+    dm: false,
+    abs: false,
+    ntf: false,
+    imp: false,
+    xor: false,
+    selectAll: false
+  };
+  var load = function() {
+    //attemp to retrieve checkboxDict from localStorage
+    var item = localStorage.getItem("checkBoxDict");
+    if (item != null) {
+      checkBoxDict = JSON.parse(item);
+    }
+  };
+  var sync = function() {
+    console.log("Syncing storage and ui");
+    //set checkboxes to match dict
+    for (var key in checkBoxDict) {
+      document.getElementById(key).checked = checkBoxDict[key];
+    }
+  }
+  this.save = function() {
+    //save checkboxdict to localStorage
+    localStorage.setItem("checkBoxDict", JSON.stringify(checkBoxDict))
+  }
+  //load data and setup
+  load();
+  sync();
 };
